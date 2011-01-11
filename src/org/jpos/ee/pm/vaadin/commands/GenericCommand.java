0503cbd123d6f4e48f85ceba54dd803e6671e7ae
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-01-11 15:26:39 -0300

    Added operations to list

--- src/org/jpos/ee/pm/vaadin/commands/GenericCommand.java
+++ src/org/jpos/ee/pm/vaadin/commands/GenericCommand.java
@@ -1,10 +1,21 @@
 package org.jpos.ee.pm.vaadin.commands;
 
+import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Layout;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
+import org.jpos.ee.pm.core.Entity;
+import org.jpos.ee.pm.core.Operation;
+import org.jpos.ee.pm.core.Operations;
 import org.jpos.ee.pm.core.PMContext;
 import org.jpos.ee.pm.core.PMException;
 import org.jpos.ee.pm.core.PresentationManager;
+import org.jpos.ee.pm.core.operations.OperationCommandSupport;
+import org.jpos.ee.pm.vaadin.components.PMMainWindow;
 
 /**
  *
@@ -12,12 +23,39 @@ import org.jpos.ee.pm.core.PresentationManager;
  */
 public abstract class GenericCommand {
 
+    public static final String WINDOW = "window";
     private String operationId;
     private PMContext ctx;
 
     public GenericCommand(PMContext ctx) {
         this.operationId = ctx.getString("operation");
         this.ctx = ctx;
+    }
+
+    protected void createOperationBar(VerticalLayout vl, String... scopes) throws PMException {
+        HorizontalLayout bar = new HorizontalLayout();
+        Operations opers = getCtx().getEntity().getOperations().getOperationsFor(getCtx().getOperation());
+        if (scopes != null && scopes.length > 0) {
+            opers = opers.getOperationsForScope(scopes);
+        }
+        for (final Operation operation : opers.getOperations()) {
+            final Button button = new Button(operation.getId());
+            button.setIcon(new ThemeResource("./operations/" + operation.getId() + ".gif"));
+            bar.addComponent(button);
+            button.addListener(new Button.ClickListener() {
+
+                public void buttonClick(ClickEvent event) {
+                    final PMContext c = new PMContext(getCtx().getSessionId());
+                    c.put(OperationCommandSupport.PM_ID, getCtx().get("entity"));
+                    c.put(WINDOW, getCtx().get(WINDOW));
+                    c.put("new", false);
+                    final PMMainWindow window = (PMMainWindow) ctx.get(WINDOW);
+                    final GenericCommand cmd = CommandFactory.newCommand(operation.getId(), c);
+                    window.setMainScreen(cmd.execute());
+                }
+            });
+        }
+        vl.addComponent(bar);
     }
 
     public abstract HorizontalLayout execute();
@@ -30,9 +68,24 @@ public abstract class GenericCommand {
         return ctx;
     }
 
+    protected void createTitle(final Layout vl) throws PMException {
+        final Label title = new Label("<h2>" + getOperationTitle() + "</h2>");
+        title.setContentMode(Label.CONTENT_XHTML);
+        vl.addComponent(title);
+    }
+
     protected HorizontalLayout getErrorLayout(PMException ex) {
         ex.printStackTrace();
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    protected void finalPanel(HorizontalLayout l, VerticalLayout vl) {
+        Panel p = new Panel();
+        p.setStyle(Panel.STYLE_LIGHT);
+        p.setScrollable(true);
+        p.setSizeFull();
+        p.setContent(vl);
+        l.addComponent(p);
     }
 
     protected Panel newOperationPanel() throws PMException {
@@ -42,6 +95,10 @@ public abstract class GenericCommand {
     }
 
     protected String getOperationTitle() throws PMException {
-        return PresentationManager.getMessage("pm.entity." + getCtx().getEntity().getId()) + "(" + PresentationManager.getMessage("operation." + getCtx().getOperation().getId()) + ")";
+        return getOperationTitle(getCtx().getEntity(), getCtx().getOperation());
+    }
+
+    protected String getOperationTitle(final Entity entity, final Operation operation) throws PMException {
+        return PresentationManager.getMessage("pm.entity." + entity.getId()) + "(" + PresentationManager.getMessage("operation." + operation.getId()) + ")";
     }
 }
--- src/org/jpos/ee/pm/vaadin/commands/ListCommand.java
+++ src/org/jpos/ee/pm/vaadin/commands/ListCommand.java
@@ -1,17 +1,21 @@
 package org.jpos.ee.pm.vaadin.commands;
 
+import com.vaadin.event.Action;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
+import org.jpos.ee.pm.core.Operation;
+import org.jpos.ee.pm.core.Operations;
 import org.jpos.ee.pm.core.PMContext;
 import org.jpos.ee.pm.core.PMException;
 import org.jpos.ee.pm.core.PresentationManager;
 import org.jpos.ee.pm.core.operations.ListOperation;
 import org.jpos.ee.pm.core.operations.OperationCommandSupport;
 import org.jpos.ee.pm.vaadin.components.ListContainer;
+import org.jpos.ee.pm.vaadin.components.PMMainWindow;
 import org.jpos.ee.pm.vaadin.components.TableNavForm;
 
 /**
@@ -41,18 +45,50 @@ public class ListCommand extends GenericCommand {
             HorizontalLayout l = new HorizontalLayout();
             l.setSizeFull();
             VerticalLayout vl = new VerticalLayout();
-            vl.setSpacing(true);
-            vl.setMargin(true, true, true, true);
-
-            final Label title = new Label("<h2>" + getOperationTitle() + "</h2>");
-            title.setContentMode(Label.CONTENT_XHTML);
-            vl.addComponent(title);
+            //vl.setSpacing(true);
+            vl.setMargin(false, true, true, true);
+            createTitle(vl);
+            createOperationBar(vl, Operation.SCOPE_GRAL);
 
             final Table table = new Table();
             table.setContainerDataSource(new ListContainer(getCtx().getList()));
             table.setHeight("150px");
-            table.setPageLength((Integer)getCtx().get("rows_per_page"));
+            table.setPageLength(getCtx().getList().getRowsPerPage());
             table.setSizeFull();
+
+            table.addActionHandler(new Action.Handler() {
+
+                public Action[] getActions(Object target, Object sender) {
+                    try {
+                        final Operations itemOperations = getMyOperations().getItemOperations();
+                        final Action[] actions = new Action[itemOperations.getOperations().size()];
+                        int i = 0;
+                        for (Operation operation : itemOperations.getOperations()) {
+                            PMContext c = new PMContext(getCtx().getSessionId());
+                            c.put(OperationCommandSupport.PM_ID, getCtx().get("entity"));
+                            c.put(OperationCommandSupport.PM_ITEM, target.toString());
+                            c.put(WINDOW, getCtx().get(WINDOW));
+                            actions[i] = new LocalListAction(c, operation);
+                            i++;
+                        }
+                        return actions;
+                    } catch (PMException ex) {
+                        PresentationManager.getPm().error(ex);
+                    }
+                    return null;
+                }
+
+                protected Operations getMyOperations() throws PMException {
+                    return getCtx().getEntity().getOperations().getOperationsFor(getCtx().getOperation());
+                }
+
+                public void handleAction(Action action, Object sender, Object target) {
+                    LocalListAction lla = (LocalListAction)action;
+                    lla.doIt();
+                }
+            });
+
+
             vl.addComponent(table);
 
 
@@ -60,17 +96,32 @@ public class ListCommand extends GenericCommand {
             vl.addComponent(navform);
             vl.setComponentAlignment(navform, Alignment.BOTTOM_CENTER);
 
-            Panel p = new Panel();
-            p.setStyle(Panel.STYLE_LIGHT);
-            p.setScrollable(true);
-            p.setSizeFull();
-            p.setContent(vl);
-            l.addComponent(p);
+            finalPanel(l, vl);
 
             return l;
         } catch (PMException ex) {
             PresentationManager.getPm().error(ex);
             return getErrorLayout(ex);
+        }
+    }
+
+    private class LocalListAction extends Action {
+        private PMMainWindow window;
+        private GenericCommand command;
+
+        public LocalListAction(String caption) {
+            super(caption);
+        }
+
+        public LocalListAction(PMContext ctx, Operation operation) throws PMException {
+            super(PresentationManager.getMessage("operation."+operation.getId()));
+            window = (PMMainWindow) ctx.get(WINDOW);
+            command = CommandFactory.newCommand(operation.getId(), ctx);
+        }
+
+        public void doIt() {
+            final HorizontalLayout l = command.execute();
+            window.setMainScreen(l);
         }
     }
 }
