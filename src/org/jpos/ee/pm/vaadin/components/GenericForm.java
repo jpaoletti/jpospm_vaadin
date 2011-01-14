5e68297ddc4c9451ecac2ab5919bc417191ee50c
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-01-14 15:13:07 -0300

    import cleanup

--- src/org/jpos/ee/pm/vaadin/components/GenericForm.java
+++ src/org/jpos/ee/pm/vaadin/components/GenericForm.java
@@ -1,16 +1,9 @@
 package org.jpos.ee.pm.vaadin.components;
 
-import com.vaadin.data.Item;
-import com.vaadin.data.util.BeanItem;
 import com.vaadin.ui.*;
-import java.util.ArrayList;
-import java.util.List;
-import org.jpos.ee.pm.core.Entity;
-import org.jpos.ee.pm.core.Operation;
 import org.jpos.ee.pm.core.PMContext;
 import org.jpos.ee.pm.core.PMCoreObject;
 import org.jpos.ee.pm.core.PMException;
-import org.jpos.ee.pm.core.PresentationManager;
 import org.jpos.ee.pm.vaadin.VaadinSupport;
 
 /**
