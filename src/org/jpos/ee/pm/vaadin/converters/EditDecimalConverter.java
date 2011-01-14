918ea6f8764ce9c6ab4494141bfb16207c71cc43
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-01-14 15:12:53 -0300

    Added a vaadin validator to decimal converter.

--- src/org/jpos/ee/pm/vaadin/converters/EditDecimalConverter.java
+++ src/org/jpos/ee/pm/vaadin/converters/EditDecimalConverter.java
@@ -1,9 +1,11 @@
 package org.jpos.ee.pm.vaadin.converters;
 
+import com.vaadin.data.Validator;
+import com.vaadin.ui.TextField;
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import org.jpos.ee.pm.converter.ConverterException;
 import org.jpos.ee.pm.core.PMContext;
+import org.jpos.ee.pm.core.PresentationManager;
 
 /**
  *
@@ -19,5 +21,30 @@ public class EditDecimalConverter extends VaadinEditStringConverter {
         } catch (Exception e) {
             throw new ConverterException(e);
         }
+    }
+
+    @Override
+    public Object visualize(PMContext ctx) throws ConverterException {
+        final TextField f = (TextField)super.visualize(ctx);
+        f.addValidator(new Validator() {
+
+            public void validate(Object value) throws InvalidValueException {
+                try {
+                    new BigDecimal(value.toString());
+                } catch (Exception e) {
+                    throw new InvalidValueException(PresentationManager.getMessage("invalid.decimal"));
+                }
+            }
+
+            public boolean isValid(Object value) {
+                try {
+                    validate(value);
+                    return true;
+                } catch (Exception e) {
+                    return false;
+                }
+            }
+        });
+        return f;
     }
 }
