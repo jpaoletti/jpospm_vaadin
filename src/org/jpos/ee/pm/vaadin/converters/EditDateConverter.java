cbe952251ff5617aa2f155665eb924c094c97412
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-01-14 12:35:17 -0300

    A date is a date

--- src/org/jpos/ee/pm/vaadin/converters/EditDateConverter.java
+++ src/org/jpos/ee/pm/vaadin/converters/EditDateConverter.java
@@ -1,9 +1,7 @@
 package org.jpos.ee.pm.vaadin.converters;
 
-import com.vaadin.ui.DateField;
 import com.vaadin.ui.PopupDateField;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import org.jpos.ee.pm.converter.ConverterException;
 import org.jpos.ee.pm.core.PMContext;
@@ -16,15 +14,7 @@ public class EditDateConverter extends VaadinConverter {
 
     @Override
     public Object build(PMContext ctx) throws ConverterException {
-        try {
-            String value = ctx.getString(PM_FIELD_VALUE);
-            if (value != null) {
-                return getDateFormat().parse((String) value);
-            }
-        } catch (ParseException e) {
-            throw new ConverterException(e);
-        }
-        return null;
+        return ctx.get(PM_FIELD_VALUE);
     }
 
     @Override
